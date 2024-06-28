db34009a34578a71cdda06f01d303d05e051454b
dadak-dom <ddadak@wesleyan.edu>
2024-06-28 13:28:44 -0400

    Update preferences, add new argument processing (#45)

---
+++ selenium-crawler/constants.js
@@ -0,0 +1,27 @@
+/*
+
+This file contains all of the hard-coded values that need to be used by the crawler.
+
+*/
+
+// Location values for each location that we will be crawling.
+// More information regarding this issue can be found at https://github.com/privacy-tech-lab/privacy-pioneer-web-crawler#81-gps-coordinates-and-zip-codes-when-using-vpns
+// These values were obtained during validation by manually inspecting the data taken from various IP-based geolocation APIs.
+
+const LOCATION_VALUES = {
+  IOWA: {
+    lat: 41.2591,
+    long: -95.8517,
+    zip: "51502",
+  },
+};
+
+// Time constants
+const ONE_MINUTE_IN_MS = 60000;
+const FOREVER = 2147483647;
+
+module.exports = {
+  LOCATION_VALUES,
+  ONE_MINUTE_IN_MS,
+  FOREVER,
+};
